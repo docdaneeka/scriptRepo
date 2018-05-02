f27b0781235fba2f3ef9192f88aa62c4f8016b56
Aleksander <alex@MacBookeksander.mydomain.example>
2018-05-02 21:09:26 +0200

    add script

---
+++ DummyRule.groovy
@@ -0,0 +1,15 @@
+import re.hub.commithub.rest.commit.CommitCheckRequest
+import re.hub.commithub.ruleScriptAnnotations.RuleParameter
+import re.hub.commithub.ruleScriptAnnotations.RuleScript
+import re.hub.commithub.ruleScriptAnnotations.RuleParameterValue
+
+@RuleScript(ruleName = "Script from repo", ruleDescription = "Put a string here: {{astring}} ")
+class DummyRule {
+
+    @RuleParameter(type=RuleParameter.Type.STRING, name="astring", required=true)
+    String astring = "defaultvalue"
+
+    boolean check(CommitCheckRequest commitCheckRequest, OutputStream outputCommitMessage) {
+        return true;
+    }
+}
